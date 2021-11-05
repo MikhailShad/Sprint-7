934d157c8968921761ca4d19c52be86407098c7a
Mikhail Shadrin <shadrin.michael@yandex.ru>
2021-11-05 17:17:14 +0300

    Added tests

--- spring-boot/src/main/kotlin/com/example/demo/persistence/entity/Entity.kt
+++ spring-boot/src/main/kotlin/com/example/demo/persistence/entity/Entity.kt
@@ -1,18 +1,10 @@
 package com.example.demo.persistence.entity
 
-import javax.persistence.Column
+import javax.persistence.*
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
 
 @Entity
 class Entity(
-    @Id
-    @GeneratedValue
-    var id: Long?,
-
-    @Column
-    var name: String?
-) {
-
-}
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+    @Column var name: String?
+)
